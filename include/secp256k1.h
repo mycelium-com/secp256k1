614d984c5e580ec3fcf37bbe3f6fbb072688e9d1
CryptoManiac <balthazar@yandex.ru>
2020-08-13 21:11:56 +0300

    Implement secp256k1_add_scalar API

--- include/secp256k1.h
+++ include/secp256k1.h
@@ -12,6 +12,7 @@ void secp256k1_create_privkey(unsigned char *private_key, const unsigned char *s
 void secp256k1_create_keypair(unsigned char *public_key, unsigned char *private_key, const unsigned char *seed);
 void secp256k1_sign(unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key, const unsigned char *private_key);
 int secp256k1_verify(const unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key);
+int secp256k1_add_scalar(unsigned char *public_key, unsigned char *private_key, const unsigned char *scalar);
 
 #ifdef __cplusplus
 }
--- src/add_scalar.c
+++ src/add_scalar.c
@@ -1,5 +1,28 @@
 #include "secp256k1.h"
+#include "uECC.h"
 
-void secp256k1_add_scalar(unsigned char *public_key, unsigned char *private_key, const unsigned char *scalar) {
-    // Stub
+int secp256k1_add_scalar(unsigned char *public_key, unsigned char *private_key, const unsigned char *scalar) {
+    const struct uECC_Curve_t* secp256k1 = uECC_secp256k1();
+
+    // Space for key decompression
+    unsigned char decompressed[64];
+
+    int failed = 0;
+
+    if (public_key) {
+        // Keys must be uncompressed first
+        uECC_decompress(public_key, decompressed, secp256k1);
+
+        // Tweak public key
+        failed |= !uECC_public_point_tweak(decompressed, decompressed, scalar, secp256k1);
+
+        // Compress it back
+        uECC_compress(decompressed, public_key, secp256k1);
+    }
+
+    if (private_key) {
+        failed |= !uECC_private_scalar_tweak(private_key, private_key, scalar, secp256k1);
+    }
+
+    return !failed;
 }
