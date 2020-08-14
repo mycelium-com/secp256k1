5f4547e730521dd135b93aca01b8d175f34813d0
CryptoManiac <balthazar@yandex.ru>
2020-08-14 21:04:12 +0300

    Update micro-secp256k1

--- src/sign.c
+++ src/sign.c
@@ -1,42 +1,35 @@
 #include "secp256k1.h"
-#include "sha3.h"
+#include "hmac_sha3.h"
 #include "uECC.h"
 
-typedef struct HashContext {
-    uECC_HashContext uECC;
-    SHA3_CTX ctx;
-} HashContext;
-
-static void init_hash(const uECC_HashContext *base) {
-    HashContext *context = (HashContext *)base;
-    sha3_256_Init(&context->ctx);
+static void init_HMAC(const struct uECC_HashContext *base, const uint8_t *key, int key_len) {
+    hmac_sha3_256_init((hmac_sha3_256_ctx *)base->ctx, key, key_len);
 }
 
-static void update_hash(const uECC_HashContext *base, const uint8_t *message, unsigned message_size) {
-    HashContext *context = (HashContext *)base;
-    sha3_Update(&context->ctx, message, message_size);
+static void update_HMAC(const struct uECC_HashContext *base, const uint8_t *data, int len) {
+    hmac_sha3_256_update((hmac_sha3_256_ctx *)base->ctx, data, len);
 }
 
-static void finish_hash(const uECC_HashContext *base, uint8_t *hash_result) {
-    HashContext *context = (HashContext *)base;
-    sha3_Final(&context->ctx, hash_result);
+static void finish_HMAC(const struct uECC_HashContext *base, uint8_t *digest) {
+    hmac_sha3_256_final((hmac_sha3_256_ctx *) base->ctx, digest, SHA3_256_DIGEST_LENGTH);
 }
 
 void secp256k1_sign(unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key, const unsigned char *private_key) {
 
     uint8_t tmpsig[64];
-    uint8_t tmp[2 * SHA3_256_DIGEST_LENGTH + SHA3_256_BLOCK_LENGTH];
-    HashContext ctx = {{
-        &init_hash,
-        &update_hash,
-        &finish_hash,
-        SHA3_256_BLOCK_LENGTH,
+    uint8_t tmp[2 * SHA3_256_DIGEST_LENGTH + 1];
+    hmac_sha3_256_ctx hmac_ctx;
+    uECC_HashContext ctx = {
+        &init_HMAC,
+        &update_HMAC,
+        &finish_HMAC,
+        &hmac_ctx,
         SHA3_256_DIGEST_LENGTH,
         tmp
-    }};
+    };
 
     // Generate deterministic signature
-    uECC_sign_deterministic(private_key, message, message_len, &ctx.uECC, tmpsig);
+    uECC_sign_deterministic(private_key, message, message_len, &ctx, tmpsig);
 
     // Serialize to DER
     uECC_compact_to_der(tmpsig, signature);
