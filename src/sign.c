57c2c3ad27278cd3ca57b2c3bf88f88da6595e41
CryptoManiac <balthazar.ad@gmail.com>
2021-11-05 09:38:24 +0300

    Add mycelum prefix to hash functions

--- src/sign.c
+++ src/sign.c
@@ -3,22 +3,22 @@
 #include "uECC.h"
 
 static void init_HMAC(const struct uECC_HashContext *base, const uint8_t *key, int key_len) {
-    hmac_sha3_256_init((hmac_sha3_256_ctx *)base->ctx, key, key_len);
+    myc_hmac_sha3_256_init((myc_hmac_sha3_256_ctx *)base->ctx, key, key_len);
 }
 
 static void update_HMAC(const struct uECC_HashContext *base, const uint8_t *data, int len) {
-    hmac_sha3_256_update((hmac_sha3_256_ctx *)base->ctx, data, len);
+    myc_hmac_sha3_256_update((myc_hmac_sha3_256_ctx *)base->ctx, data, len);
 }
 
 static void finish_HMAC(const struct uECC_HashContext *base, uint8_t *digest) {
-    hmac_sha3_256_final((hmac_sha3_256_ctx *) base->ctx, digest, SHA3_256_DIGEST_LENGTH);
+    myc_hmac_sha3_256_final((myc_hmac_sha3_256_ctx *) base->ctx, digest, SHA3_256_DIGEST_LENGTH);
 }
 
 int secp256k1_sign(unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *private_key) {
 
     uint8_t tmpsig[64];
-    uint8_t tmp[2 * SHA3_256_DIGEST_LENGTH + 1];
-    hmac_sha3_256_ctx hmac_ctx;
+    uint8_t tmp[2 * MYC_SHA3_256_DIGEST_LENGTH + 1];
+    myc_hmac_sha3_256_ctx hmac_ctx;
     uECC_HashContext ctx = {
         &init_HMAC,
         &update_HMAC,
